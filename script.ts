1856b2c3bbdf1fb90b745e4857c2a33f0e16d855
Monika Lie <104011338+lunadragon06@users.noreply.github.com>
2024-01-28 19:00:19 +0100

    Create script.ts

---
+++ script.ts
@@ -0,0 +1,17 @@
+export {};
+
+const first = document.getElementById("firstMeter") as HTMLInputElement;
+const second = document.getElementById("secondMeter") as HTMLInputElement;
+const result = document.getElementById("result") as HTMLElement;
+
+first.addEventListener("input", sum);
+second.addEventListener("input", sum);
+
+function sum(): void {
+  const firstMeter: number = parseFloat(first.value) || 0;
+  const secondMeter: number = parseFloat(second.value) || 0;
+
+  const squareMeters = firstMeter * secondMeter;
+  result.innerHTML = squareMeters.toFixed(2) + " m².";
+  console.log(squareMeters.toFixed(2) + " m².");
+}
